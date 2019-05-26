1a55a2b3e4796bedd789e27a52be59ac315fe3d4
Caetano Hyams <cjhyams@ucsc.edu>
2019-05-25 21:14:46 -0700

    No longer stable, rounds functionality in progress

--- finalGame/js/states/GameOver.js
+++ finalGame/js/states/GameOver.js
@@ -8,13 +8,10 @@ GameOver.prototype = {
 		this.loser = loser;
 		this.score = score;
 
-		if (this.winner.player == 1)
-		{
-			this.score.push("earth"); 
-		}
-		else if (this.winner == 2)
-		{
-			this.score.push("mars");
+		if (this.winner.player == 1) {
+			this.score[1] += 1;
+		} else if (this.winner.player == 2) {
+			this.score[2] += 1;
 		}
 	},
 	preload: function() {
@@ -26,18 +23,18 @@ GameOver.prototype = {
 		//game.camera.focusOn(this.winner);
 		game.camera.follow(this.winner, 0.1, 0.1);
 		this.winner.body.bounce.set(0);
-		
+
 		var text = "Player " + this.winner.player + " wins!\nPress space to play again";
 		var gameOverText = game.add.text(game.world.centerX - 150, game.world.centerY - 50, text, { fontSize: '30px', fill: '#ffffff', align: 'center'});
-},
+	},
 	update: function() {
-   		if (game.physics.arcade.collide(game.debris, game.players)){
+   		if (game.physics.arcade.collide(game.debris, game.players)) {
    			console.log('bonk');
    		}
 
-		if(game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)){
+		if(game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
 			game.state.start("Play", true, false, false);
-		} else if(game.input.keyboard.isDown(Phaser.Keyboard.ENTER)){
+		} else if(game.input.keyboard.isDown(Phaser.Keyboard.ENTER)) {
 			game.state.start("Play", true, false, true);
 		}
 	}
